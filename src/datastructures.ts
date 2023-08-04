81a1035c9efad51e26fc479969be973a630a2aca
Daniel Lai <dlai@ufl.edu>
2023-08-04 01:37:52 -0400

    added MaxHeap class, may need tweaking

---
+++ src/datastructures.ts
@@ -0,0 +1,76 @@
+export class maxHeapBased<T>{
+    mapNodes: { priority: number;  data: T } [];
+    
+    constructor() {
+        this.mapNodes = [];
+    }
+
+    push(node: any, priority: number) {
+        this.mapNodes.push({priority: priority, data: node});
+        this.heapify(this.mapNodes.length - 1);
+    }
+
+    pop(): T | undefined {
+        if(this.mapNodes.length === 0) {
+            return undefined;
+        }
+        const result = this.mapNodes[0].data;
+        const end = this.mapNodes.pop();
+        if(this.mapNodes.length > 0 && end) {
+            this.mapNodes[0] = end;
+            this.heapifyDown(0);
+        }
+        return result;
+    }
+
+    peek(): any {
+        if(this.mapNodes.length === 0) {
+            return undefined;
+        }
+        return this.mapNodes[0].data;
+    }
+    
+    size(): number {
+        return this.mapNodes.length;
+    }
+
+    isEmpty(): boolean {
+        return this.mapNodes.length === 0;
+    }
+
+    top(): T[] {
+        let topCities: T[] = [];
+        for (let i = 0; i < 15; i++) {
+            topCities.push(this.mapNodes[i].data);
+        }
+
+        for (let i = 0; i < topCities.length; i++) {
+            console.log(topCities[i]);
+        }
+        return topCities;
+    }
+
+    heapify(index: number) {
+        let parent = Math.floor((index - 1) / 2);
+        if (this.mapNodes[parent] && this.mapNodes[index].priority > this.mapNodes[parent].priority) {
+            [this.mapNodes[parent], this.mapNodes[index]] = [this.mapNodes[index], this.mapNodes[parent]];
+            this.heapify(parent);
+        }
+    }
+
+    heapifyDown(index: number) {
+        let left = 2 * index + 1;
+        let right = 2 * index + 2;
+        let largest = index;
+        if (this.mapNodes[left] && this.mapNodes[left].priority > this.mapNodes[largest].priority) {
+            largest = left;
+        }
+        if (this.mapNodes[right] && this.mapNodes[right].priority > this.mapNodes[largest].priority) {
+            largest = right;
+        }
+        if (largest != index) {
+            [this.mapNodes[index], this.mapNodes[largest]] = [this.mapNodes[largest], this.mapNodes[index]];
+            this.heapifyDown(largest);
+        }
+    }
+}
---
+++ src/test.ts
@@ -0,0 +1,26 @@
+import { maxHeapBased } from './datastructures';
+
+const test = new maxHeapBased();
+
+test.push('a', 1);
+test.push('b', 2);
+test.push('c', 3);
+test.push('d', 4);
+test.push('e', 5);
+test.push('f', 6);
+test.push('g', 7);
+test.push('h', 8);
+test.push('i', 9);
+test.push('j', 10);
+test.push('k', 11);
+test.push('l', 12);
+test.push('m', 13);
+test.push('n', 14);
+test.push('o', 15);
+test.push('p', 16);
+test.push('q', 17);
+test.push('r', 18);
+test.push('s', 19);
+test.push('t', 20);
+
+test.top();
